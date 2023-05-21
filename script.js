72c0de175daeb524a83e8d08232ad5a478f2581d
Kelvin-Ben <kevoben14@gmail.com>
2023-05-21 23:50:12 +0300

    Dynamically rendered the workout list

--- script.js
+++ script.js
@@ -143,8 +143,7 @@ class App {
     // Render workout on the map as marker
     this._renderWorkoutMarker(workout);
 
-    // Render workout on list
-    
+   
     // clear inputs
     inputCadence.value =
       inputDistance.value =
@@ -168,5 +167,55 @@ class App {
       .setPopupContent("Workout")
       .openPopup();
         }
+         // Render workout on list
+    _renderWorkout(workout) {
+      let html = `
+      <li class="workout workout--${workout.type}" data-id=${workout.id}>
+          <h2 class="workout__title">${workout.description}</h2>
+          <div class="workout__details">
+            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}</span>
+            <span class="workout__value">${workout.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">24</span>
+            <span class="workout__unit">min</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.duration}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+      `;
+      if (workout.type === 'running') 
+        html =+ `
+        <div class="workout__details">
+        <span class="workout__icon">⚡️</span>
+        <span class="workout__value">${workout.pace.toFixed(1)}</span>
+        <span class="workout__unit">min/km</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">🦶🏼</span>
+        <span class="workout__value">178</span>
+        <span class="workout__unit">spm</span>
+      </div>
+    </li>
+        `;
+      if (workout.type === 'cycling')
+      html += `
+      <div class="workout__details">
+      <span class="workout__icon">⚡️</span>
+      <span class="workout__value">${workout.speed}</span>
+      <span class="workout__unit">km/h</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">⛰</span>
+      <span class="workout__value">${workout.elevationGain}</span>
+      <span class="workout__unit">m</span>
+    </div>
+  </li> 
+      `
+    }
 }
 const app = new App();
